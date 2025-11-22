6119487c51c0fba6057829d8c2aadba509205e01
Hai <sokhai.cambodia@gmail.com>
2025-11-22 16:38:00 +0700

    add grant_select

---
+++ sql/dwh/03_gold/grant_select_silver_to_gold.sql
@@ -0,0 +1,6 @@
+GRANT SELECT ON silver_usr.crm_cust_info TO gold_usr;
+GRANT SELECT ON silver_usr.crm_prd_info TO gold_usr;
+GRANT SELECT ON silver_usr.crm_sales_details TO gold_usr;
+GRANT SELECT ON silver_usr.erp_cust_info TO gold_usr;
+GRANT SELECT ON silver_usr.erp_location TO gold_usr;
+GRANT SELECT ON silver_usr.erp_category TO gold_usr;
